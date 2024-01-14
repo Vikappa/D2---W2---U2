f16c75177998a75311810326946dc2dbf96fc295
Vikappa <vincenzo.e.costantini@gmail.com>
2024-01-14 03:19:11 +0100

    Fine fine

--- assets/script/scripts.js
+++ assets/script/scripts.js
@@ -62,17 +62,13 @@ coloreTesto()
 let interValAggiornaTesto = setInterval(aggiornatesto, 100)
 let interValDiscoteca = setInterval(coloreTesto, 100)
 
-let items = document.querySelectorAll('.carousel .carousel-item')
-
-items.forEach((el) => {
-    const minPerSlide = 4
-    let next = el.nextElementSibling
-    for (var i = 1; i < minPerSlide; i++) {
-        if (!next) {
-            next = items[0]
-        }
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
+$('.carousel .carousel-item').each(function () {
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
     }
-})
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) { next = next.next(); if (!next.length) { next = $(this).siblings(':first'); } next.children(':first-child').clone().appendTo($(this)); }
+});
